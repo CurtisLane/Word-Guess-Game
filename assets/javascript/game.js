41f26f75a675ea583621082dab175ad9150a0f69
Curtis Lane <clanelmt@gmail.com>
2020-04-09 11:49:31 -0400

    add you win message when answer is completed correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,11 @@ const words = ["Piccolo", "Gohan", "Goku", "Bulma","Vegeta", "Trunks", "Goten", 
 // word chosen at random
 let randomNumber = Math.floor(Math.random() * words.length);
 let randomWord = words[randomNumber];
-// blank underscore
+
+// blank underscore to be filled by correct letters and wrong guesses
 let blank = [];
 let wrongGuesses = [];
+let wins = 0;
 
 console.log(randomWord);
 // make underscore for each letter of randomWord in index.html
@@ -15,28 +17,33 @@ let underscores = function()  {
     }
     return blank;
 }
-console.log(underscores());
-
+// console.log(underscores());
+underscores();
 // listen for keypress
 document.addEventListener('keypress', (event) => {
     let keyNumber = event.keyCode;
     let keyLetter = String.fromCharCode(keyNumber);
     // if letter matches, put letter into blank array
     for (i=0; i<randomWord.length; i++){
-        // check for correct answers
+        // check for correct answers, replace underscore with correct letter
         if (keyLetter.toLowerCase() === randomWord.toLowerCase().charAt(i)) {
             blank.splice(i, 1,  keyLetter);
             console.log(blank);
         } 
+    //tried to make else statement for incorrect answers but then several of the same letter got pushed to wrongGuesses
     };
-    // check for incorrect answers
+    // check for incorrect answers using indexOf which === -1 when no matches are found
     if (randomWord.toLowerCase().indexOf(keyLetter.toLowerCase()) === -1) {
         wrongGuesses.push(keyLetter);
         console.log(wrongGuesses)
     };
+    if (blank.join('') === randomWord.toLowerCase()) {
+        console.log("You Win!")
+        wins = wins + 1;
+        console.log(wins);
 
+    }
 });
-
 
 
 
