a728dc6a8a56a6175b8735a4360c4bd13704171d
Curtis Lane <clanelmt@gmail.com>
2020-04-09 00:12:28 -0400

    Add first const to game.js, add event listener.

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+const words = ["Piccolo", "Gohan", "Goku", "Bulma","Vegeta", "Trunks", "Goten", "Krillin", "Tien", "Yamcha"]
+
+
+
+
+
+
+
+// I did a thing! Woo my first javascript!!!
+document.addEventListener("keypress", function(){
+  document.getElementById("firstLetter").innerHTML = "P";
+});
