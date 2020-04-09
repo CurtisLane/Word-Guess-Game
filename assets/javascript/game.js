266432cfda6cf10175ce2be55e34def6db87749c
Curtis Lane <clanelmt@gmail.com>
2020-04-09 01:11:49 -0400

    add variables and event listener to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,31 @@
+// array of words to guess
 const words = ["Piccolo", "Gohan", "Goku", "Bulma","Vegeta", "Trunks", "Goten", "Krillin", "Tien", "Yamcha"]
+// word chosen at random
+let randomNumber = Math.floor(Math.random() * words.length);
+let randomWord = words[randomNumber];
+// blank underscore
+let blank = [];
+console.log(randomWord);
+// make underscore for each letter of randomWord in index.html
+let underscores = function()  {
+    for(let i = 0; i < randomWord.length; i++) {
+        blank.push('_');
+    }
+    return blank;
+}
+console.log(underscores());
 
+// listen for keypress
+document.addEventListener('keypress', (event) => {
+    let keyNumber = event.keyCode;
+    let keyLetter = String.fromCharCode(keyNumber);
+    console.log(keyLetter)
+    if (keyLetter === randomWord.charAt(0)) {
+        blank.splice(0, 0, keyLetter);
+        console.log(blank);
+    };
+
+});
 
 
 
