1c13a11554f6505287447a1a72572424dacaa993
Curtis Lane <clanelmt@gmail.com>
2020-04-12 16:41:19 -0400

    clean up code on game.js, add function: spacebar to reload after losing.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,11 @@
-// Variables----------
-
 // array of words to guess
 const words = ["Piccolo", "Gohan", "Goku", "Bulma","Vegeta", "Trunks", "Goten", "Krillin", "Tien", "Yamcha"]
-// word chosen at random
-let randomWord = random_word(words);
 
-// let randomNumber = Math.floor(Math.random() * words.length);
-// let randomWord = words[randomNumber];
+// chose word from words array at random using function, assign to variable, log to console
+let randomWord = random_word(words);
 console.log(randomWord);
 
-// blank underscore to be filled by correct letters and wrong guesses
+// empty arrays and zero wins to begin game
 let blank = [];
 let wrongGuesses = [];
 let wins = 0;
@@ -17,44 +13,31 @@ let wins = 0;
 // dragon ball images array
 let dragonBallImages = [document.getElementById('oneStarBall'), document.getElementById('twoStarBall'), document.getElementById('threeStarBall'), document.getElementById('fourStarBall'), document.getElementById('fiveStarBall'), document.getElementById('sixStarBall'), document.getElementById('sevenStarBall'), ]; 
 
-
-
-
-// Functions----------
-
-// trying this randomizer from w3schools
+// function creates random word
 function random_word(words)
 {
-  
-return words[Math.floor(Math.random()*words.length)];
-     
+    return words[Math.floor(Math.random()*words.length)];   
 }
 
-// make underscore for each letter of randomWord in index.html
+// make underscore for each letter of randomWord, push to "blank" array
 let underscores = function()  {
-    
-    
     for(let i = 0; i < randomWord.length; i++) {
         blank.push('_');
     }
     return blank;
 }
 
-// event listener--------------------
-
-// listen for keypress
+// listen for user keypress
 document.addEventListener('keypress', (event) => {
     let keyNumber = event.keyCode;
-    console.log(keyNumber);
     let keyLetter = String.fromCharCode(keyNumber);
 
-    // validate input
-    
-    // remove message 
+    // remove message to user
     document.getElementById('message').innerHTML = "";
 
     // if letter matches, put letter into blank array
     for (let i=0; i<randomWord.length; i++){
+        
         // check for correct answers, replace underscore with correct letter
         if (keyLetter.toLowerCase() === randomWord.toLowerCase().charAt(i)) {
             blank.splice(i, 1,  keyLetter);
@@ -63,27 +46,28 @@ document.addEventListener('keypress', (event) => {
         } 
     };
 
+    // log user key pressed and wrong guesses array 
     console.log(keyLetter, wrongGuesses)
     console.log(randomWord.toLowerCase().indexOf(keyLetter.toLowerCase()))
+
+    //validate input to prevent duplicate wrong guesses
     if(wrongGuesses.join('').toLowerCase().indexOf(keyLetter.toLowerCase())>=0){
         console.log("Already guesssed")
-    } else
+    } else //continue below
 
-    // check for incorrect answers using indexOf which === -1 when no matches are found
-    if (randomWord.toLowerCase().indexOf(keyLetter.toLowerCase()) === -1) {
-        wrongGuesses.push(keyLetter);
-        console.log(wrongGuesses);
-        document.getElementById('wrongGuess').innerHTML = wrongGuesses.join(' ');
-        
-        // makes dragon ball images disappear when wrong answer is guessed
-        for ( let i=0; i < wrongGuesses.length ; i++ ) {
-            dragonBallImages[i].classList.add("opacityZero");          
-        } 
+        // check for incorrect answers using indexOf which === -1 when no matches are found
+        if (randomWord.toLowerCase().indexOf(keyLetter.toLowerCase()) === -1) {
+            wrongGuesses.push(keyLetter);
+            console.log(wrongGuesses);
+            document.getElementById('wrongGuess').innerHTML = wrongGuesses.join(' ');
+            
+            // makes dragon ball images disappear when wrong answer is guessed
+            for ( let i=0; i < wrongGuesses.length ; i++ ) {
+                dragonBallImages[i].classList.add("opacityZero");          
+            } 
     } 
 
-
-
-    //the player wins after guessing all the correct letters and gets a message then it resets
+    // player wins after guessing all correct letters, message to user is pushed to HTML
     if (blank.join('') === randomWord.toLowerCase()) {
         
         // keeps track of wins
@@ -104,34 +88,28 @@ document.addEventListener('keypress', (event) => {
         document.getElementById('wrongGuess').innerHTML = ''
         blank = [];
         underscores(blank);
-        // document.getElementById('myWord').innerHTML = '';
         document.getElementById('myWord').innerHTML = blank.join(' ');
-
-        // gives answer in the console
-        // console.log(randomWord);
-
-    
-    
     }
-    // the player loses after 7 tries and gets a message
+
+    // the player loses after 7 tries, message to user pushed to HTML
     if (wrongGuesses.join('').length === 7){
-        document.getElementById('message').innerHTML = "You Lose! Frieza has the Dragon Balls and Krillin died again.."
         
-    }
+        // message to user
+        document.getElementById('message').innerHTML = "You Lose! Frieza has the Dragon Balls and Krillin died again.. Press the spacebar to retry."
+        
+        // press spacebar to reload page, copied from https://stackoverflow.com/questions/24386354/execute-js-code-after-pressing-the-spacebar
+        document.body.onkeyup = function(e){
+            if(e.keyCode == 32){
+                location.reload();
+            }
+        };
+    };
 
 });
 
-// function call----------
+// function calls
 
 underscores(blank);
 document.getElementById('myWord').innerHTML = blank.join(' ');
 
-// event listener for touch on cell phone to bring up keyboard
-// document.addEventListener("touchstart", touchHandler);
 
-function ifTouch(event) {
-    var x = event.touches;
-    if (x) {
-        alert("touch detected")
-    }
-}
